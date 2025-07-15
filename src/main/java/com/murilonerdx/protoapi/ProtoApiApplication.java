6c198c1655b2e06d4a18a5795d87f14273c6ddb5
Murilo P. S <mu-silva@outlook.com>
2025-07-15 17:50:36 -0300

    add project

---
+++ src/main/java/com/murilonerdx/protoapi/ProtoApiApplication.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.protoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/murilonerdx/protoapi/ProtoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.murilonerdx.protoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
